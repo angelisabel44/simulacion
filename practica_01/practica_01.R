cdeffb353a186ac541f47ef0d15fb6548e1e07db
ITESMangelmoreno <77754363+ITESMangelmoreno@users.noreply.github.com>
2021-02-22 08:17:59 -0600

    Tarea
    Actualización practica 1

---
+++ practica_01/practica_01.R
@@ -0,0 +1,20 @@
+f <- function(x) { return(1 / (exp(x) + exp(-x))) }
+
+simulacion_MC <- function(F, a, b, cantidad) {
+  acumumulado <- 0
+  for (i in 1:cantidad) {
+    x <- runif(1)
+    acumumulado <- acumumulado + F(a + (b - a) * x)
+  }
+  return(((b - a) * acumumulado) / cantidad)
+}
+
+desde <- 3
+hasta <- 7
+cantidad <- 50000
+replicas <- 30
+
+for (i in 1:replicas) {
+  integral <- simulacion_MC(f, desde, hasta, cantidad)
+  print(paste("Réplica ", i, ": ", integral, sep = ""))
+}
---
+++ practica_01/practica_01.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 07:32:59 2021
+
+@author: Angel Moreno
+"""
+
+from math import exp
+def f(x):
+    return 1 / (exp(x) + exp(-x))
+
+import random
+def simulacion_MC(F, a, b, cantidad):
+    acumulado = 0
+    for j in range(cantidad):
+        x = random.random()
+        acumulado += F(a + (b - a) * x)
+    return ((b - a) * acumulado)/cantidad
+
+desde = 3
+hasta = 7
+cantidad = 50000
+replicas = 30
+
+for i in range(replicas):
+    integral = simulacion_MC(f, desde, hasta, cantidad)
+    print(f'Réplica {i + 1}: {integral}')
+
